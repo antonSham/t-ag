548007c939b3ed3cc6e134420314932b8fe53064
Anton Shamyshev <saa33-saa33@yandex.ru>
2017-12-11 11:04:30 +0300

    Add home page

---
+++ t-ag/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using t_ag.Models;
+
+namespace t_ag.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.user = (User)Session["User"];
+            return View();
+        }
+    }
+}
